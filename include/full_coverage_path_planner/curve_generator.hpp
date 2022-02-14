0770ae61500f1ef3cb5424a375cd275ab4e70d97
Cesar Lopez <cesar.lopez@nobleo.nl>
2022-02-14 15:23:54 +0100

    Initial curve generator with Cubic Bezier Curve

---
+++ include/full_coverage_path_planner/curve_generator.hpp
@@ -0,0 +1,43 @@
+//
+// Copyright [2020] Nobleo Technology [legal/copyright]
+//
+#ifndef FULL_COVERAGE_PATH_PLANNER__CURVE_GENERATOR_HPP_
+#define FULL_COVERAGE_PATH_PLANNER__CURVE_GENERATOR_HPP_
+
+#include <geometry_msgs/msg/point.hpp>
+#include <nav_msgs/msg/path.hpp>
+
+#include <tf2/convert.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Vector3.h>
+
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <vector>
+
+namespace curve_generator
+{
+
+class CubicBezier2D
+{
+public:
+  CubicBezier2D() = default;
+
+  ~CubicBezier2D() = default;
+
+private:
+  /**
+   * Generate cubic Bezier curve from four control points
+   * @param p0top4 control points
+   * @param normalized_step scale value to do interpolation
+   * @param path output path
+   */
+  void generateCubicBezierCurve(
+    const tf2::Vector3 p0, const tf2::Vector3 p1,
+    const tf2::Vector3 p2, const tf2::Vector3 p3,
+    const double normalized_step, nav_msgs::msg::Path & path);
+};
+}  // namespace curve_generator
+
+#endif  // FULL_COVERAGE_PATH_PLANNER__CURVE_GENERATOR_HPP_
---
+++ src/curve_generator.cpp
@@ -0,0 +1,54 @@
+//
+// Copyright [2020] Nobleo Technology [legal/copyright]
+//
+#include "full_coverage_path_planner/curve_generator.hpp"
+
+namespace curve_generator
+{
+
+// Generate Cubic BezierCurve using the logic in these sources
+// Link 1: https://en.wikipedia.org/wiki/B%C3%A9zier_curve#/media/File:B%C3%A9zier_3_big.svg
+// Link 2:
+// https://en.wikipedia.org/wiki/B%C3%A9zier_curve#
+// Link 3:
+// https://stackoverflow.com/questions/785097/how-do-i-implement-a-b%C3%A9zier-curve-in-c
+// Link 4:
+// https://stackoverflow.com/questions/37642168/how-to-convert-quadratic-bezier-curve-code-into-cubic-bezier-curve/37642695#37642695
+// We add the calculation of the angle given by the blue lines in the gifs.
+
+void CubicBezier2D::generateCubicBezierCurve(
+  const tf2::Vector3 p0, const tf2::Vector3 p1,
+  const tf2::Vector3 p2, const tf2::Vector3 p3,
+  const double normalized_step, nav_msgs::msg::Path & path)
+{
+  for (double scale = 0.0; scale < 1.0; scale += normalized_step) {
+    // The Green Lines in Link 1
+    auto q0 = tf2::lerp(p0, p1, scale);
+    auto q1 = tf2::lerp(p1, p2, scale);
+    auto q2 = tf2::lerp(p2, p3, scale);
+
+    // The Blue Line in Link 1
+    auto r0 = tf2::lerp(q0, q1, scale);
+    auto r1 = tf2::lerp(q1, q2, scale);
+    // Vector representing the tangent of the blue line
+    auto r = r1 - r0;
+
+    // The Black Dot in Link 1
+    auto bezier_point = tf2::lerp(r0, r1, scale);
+
+    // Get pose
+    geometry_msgs::msg::PoseStamped point;
+    point.pose.position.x = bezier_point.x();
+    point.pose.position.y = bezier_point.y();
+    point.pose.position.z = bezier_point.z();
+    auto bezier_quaternion = tf2::Quaternion(r, 0.0);
+    point.pose.orientation.x = bezier_quaternion.x();
+    point.pose.orientation.y = bezier_quaternion.y();
+    point.pose.orientation.z = bezier_quaternion.z();
+    point.pose.orientation.w = bezier_quaternion.w();
+
+    path.poses.push_back(point);
+  }
+}
+
+}  // namespace curve_generator
